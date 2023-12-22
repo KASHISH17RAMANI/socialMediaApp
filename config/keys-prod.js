d0116c40f6b9df8c891d252fa840faebb47a9a13
KASHISH17RAMANI <kashishramani1703@gmail.com>
2023-12-22 16:15:08 +0530

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://soacialmediaapp:wdyowyWJfyoikc2F@onlinedatingapp.3dbkq8m.mongodb.net/?retryWrites=true&w=majority',
+    GoogleClientID: '23133228499-07rs777efrjh52hpcf7rgbkhtl58t51a.apps.googleusercontent.com',
+    GoogleClientSecret: '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'
+}
