fed4fae500dbba8d7b48d83859fd964e16d7ecf7
kashish ramani <120006509+KASHISH17RAMANI@users.noreply.github.com>
2024-02-07 10:55:08 +0530

    Add files via upload

---
+++ config/keys-dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://soacialmediaapp:wdyowyWJfyoikc2F@onlinedatingapp.3dbkq8m.mongodb.net/?retryWrites=true&w=majority',
+    GoogleClientID: '617385811520-bb6q9u83inqlhdka1v6q34u5mkmts274.apps.googleusercontent.com',
+    GoogleClientSecret: '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',
+    FacebookClientID: '909557790566645',
+    FacebookClientSecret: '0073a50e1f117a791a17506598f05863'
+}
